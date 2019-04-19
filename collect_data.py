bf2e2aa7ccab3cfa18864ce4f12a6d0bb0bc488f
helen <helentbcho@gmail.com>
2019-04-19 21:24:14 +0000

    data

--- collect_data.py
+++ collect_data.py
@@ -1,16 +1,17 @@
 import cv2     # for capturing videos
 import math   # for mathematical operations
-import pandas as pd
-from keras.preprocessing import image   # for preprocessing the images
+#import pandas as pd
+#from keras.preprocessing import image   # for preprocessing the images
 import numpy as np    # for mathematical operations
-from keras.utils import np_utils
+#from keras.utils import np_utils
 from skimage.transform import resize   # for resizing images
 
 
 count = 0
 videoFile = "Untitled.mp4"
 cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
-frameRate = cap.get(10) #frame rate
+frameRate = cap.get(5) #frame rate
+print(frameRate)
 x=1
 while(cap.isOpened()):
     frameId = cap.get(1) #current frame number
