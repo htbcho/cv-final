3190f89e62c87a0f9097d12bf5c6d3245bba1d9e
ellafeldmann <ella_feldmann@brown.edu>
2019-04-22 16:53:08 -0400

    imports

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers
+from tensorflow.keras.layers import Activation, Dense
 from tensorflow.keras import optimizers
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
