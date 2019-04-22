71acead1a134315aacfecdeeef07d3e18514a65e
Ella <ella_feldmann@brown.edu>
2019-04-22 20:22:08 +0000

    random change

--- model.py
+++ model.py
@@ -2,6 +2,6 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras import models
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 print('hello I am working')
