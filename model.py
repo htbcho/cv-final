3f509f63503a774e7d14a033f8f33ac0ca4c5888
ellafeldmann <ella_feldmann@brown.edu>
2019-04-22 16:20:03 -0400

    added a kinda sketchy network structure

--- model.py
+++ model.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras import models
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 print('hello I am working')
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(224, 224, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', input_shape=(224, 224, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512), activation='relu')
+model.add(layers.Dense(5, activation='softmax'))
