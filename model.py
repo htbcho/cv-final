b967e733cf5250e0c3b36e88b9d106bcf1f3c3d4
ellafeldmann <ella_feldmann@brown.edu>
2019-04-22 19:25:28 -0400

    jkdls;

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ from tensorflow.keras.layers import Activation, Dense
 from tensorflow.keras import optimizers
 from tensorflow.keras import models
 from tensorflow.keras import applications
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 import os
@@ -16,31 +15,27 @@ train_dir = "/home/ella_feldmann/cv-final/data"
 # validation_dir = "/home/ella_feldmann/cv-final/data"
 
 def preprocess_image(file):
-    img = image.load_img(train_dir + "/" + file, target_size=(224, 224))
+    img = image.load_img(train_dir + "/" + file)
     img_array = image.img_to_array(img)
-    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-    return applications.mobilenet.preprocess_input(img_array_expanded_dims)
-#
-# model = applications.VGG19(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, 3))
+    print(img_array.shape)
+    return img_array
+
+
 base_model = applications.mobilenet.MobileNet(weights='imagenet',include_top=False)
-# print(base_model.summary())
-#
-# x = model.output
-# x=GlobalAveragePooling2D()(x)
-# x=Dense(1024,activation='relu')(x)
-# x=Dense(1024,activation='relu')(x)
-# x=Dense(512,activation='relu')(x)
-# preds=Dense(2,activation='softmax')(x)
-#
-# model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
-#
-# step_size_train=train_generator.n//train_generator.batch_size
-# model.fit_generator(generator=train_generator,
-#                    steps_per_epoch=step_size_train,
-#                    epochs=10)
+
+x = model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x)
+x=Dense(1024,activation='relu')(x)
+x=Dense(512,activation='relu')(x)
+preds=Dense(2,activation='softmax')(x)
+
+for layer in model.layers[:20]:
+    layer.trainable=False
+for layer in model.layers[20:]:
+    layer.trainable=True
 
 for filename in os.listdir(train_dir):
     preprocessed_image = preprocess_image(filename)
     result = base_model.predict(preprocessed_image)
-    results = imagenet_utils.decode_predictions(predictions)
     print(results)
