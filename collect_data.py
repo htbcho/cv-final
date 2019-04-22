53a682e49466f9398f1e85905491fefad791e271
helen <helentbcho@gmail.com>
2019-04-22 23:46:28 +0000

    data stuff

--- collect_data.py
+++ collect_data.py
@@ -8,19 +8,19 @@ from skimage.transform import resize   # for resizing images
 
 
 count = 0
-videoFile = "Untitled.mp4"
+videoFile = "down_eugy.mov"
 cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
 frameRate = cap.get(5) #frame rate
-print(frameRate)
+#print(frameRate)
 x=1
 while(cap.isOpened()):
     frameId = cap.get(1) #current frame number
-    print(frameId)
+    #print(frameId)
     ret, frame = cap.read()
     if (ret != True):
         break
     if (frameId % 5 == 0):
-        filename ="data/frame%d.jpg" % count;count+=1
+        filename ="data/down_1_frame%d.jpg" % count;count+=1
         frame = frame[:,280:1000, :]
         frame = resize(frame, preserve_range=True, output_shape=(224,224)).astype(int)
         cv2.imwrite(filename, frame)
