839be0e07c97615bc55dba0b56302ec446fcbbd9
ellafeldmann <ella_feldmann@brown.edu>
2019-04-22 18:28:13 -0400

    mobilenet import

--- model.py
+++ model.py
@@ -11,19 +11,63 @@ img_width, img_height = 224, 224
 train_data_dir = "data/train"
 validation_data_dir = "data/val"
 
-model = applications.VGG19(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, 3))
-
+# model = applications.VGG19(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, 3))
+mobile = applications.mobilenet.MobileNet()
 print(model.summary())
+
+for layer in model.layers[:5]:
+    layer.trainable = False
+
+x = model.output
+x = Flatten()(x)
+x = Dense(1024, activation="relu")(x)
+x = Dropout(0.5)(x)
+x = Dense(1024, activation="relu")(x)
+predictions = Dense(4, activation="softmax")(x)
+
+model_final = Model(input = model.input, output = predictions)
+model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+
+# Initiate the train and test generators with data Augumentation
+# train_datagen = ImageDataGenerator(
+# rescale = 1./255,
+# horizontal_flip = True,
+# fill_mode = "nearest",
+# zoom_range = 0.3,
+# width_shift_range = 0.3,
+# height_shift_range=0.3,
+# rotation_range=30)
+#
+# test_datagen = ImageDataGenerator(
+# rescale = 1./255,
+# horizontal_flip = True,
+# fill_mode = "nearest",
+# zoom_range = 0.3,
+# width_shift_range = 0.3,
+# height_shift_range=0.3,
+# rotation_range=30)
+#
+# train_generator = train_datagen.flow_from_directory(
+# train_data_dir,
+# target_size = (img_height, img_width),
+# batch_size = batch_size,
+# class_mode = "categorical")
+#
+# validation_generator = test_datagen.flow_from_directory(
+# validation_data_dir,
+# target_size = (img_height, img_width),
+# class_mode = "categorical")
+#
+# # Save the model according to the conditions
+# checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+# early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
 #
 #
-# model = models.Sequential()
-# model.add(layers.Conv2D(32, (3, 3), Activation('relu'), input_shape=(224, 224, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), Activation('relu'), input_shape=(224, 224, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(128, (3, 3), Activation('relu'), input_shape=(224, 224, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Flatten())
-# model.add(layers.Dropout(0.5))
-# model.add(layers.Dense(512), Activation('relu'))
-# model.add(layers.Dense(5, Activation('softmax')))
+# # Train the model
+# model_final.fit_generator(
+# train_generator,
+# samples_per_epoch = nb_train_samples,
+# epochs = epochs,
+# validation_data = validation_generator,
+# nb_val_samples = nb_validation_samples,
+# callbacks = [checkpoint, early])
