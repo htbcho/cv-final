fec6ff6c1cb7eac3fbc56956b1a3d0e37213afd8
ellafeldmann <ella_feldmann@brown.edu>
2019-04-22 20:23:18 -0400

    syntax fix

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ train_generator = train_datagen.flow_from_directory(train_dir,
                                                  subset = 'training',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
-                                                 batch_size=32, # total number of training images should be divisible by batch size
+                                                 batch_size=20, # total number of training images should be divisible by batch size
                                                  class_mode='categorical',
                                                  shuffle=True)
 
@@ -51,7 +51,7 @@ valid_generator = train_datagen.flow_from_directory(train_dir,
                                                  subset = 'validation',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
-                                                 batch_size=32, # total number of training images should be divisible by batch size
+                                                 batch_size=20, # total number of training images should be divisible by batch size
                                                  class_mode='categorical',
                                                  shuffle=True)
 
@@ -62,7 +62,7 @@ valid_step_size=valid_generator.n//valid_generator.batch_size
 model.fit_generator(generator=train_generator,
                     steps_per_epoch=train_step_size,
                     validation_data=valid_generator,
-                    validation_steps=STEP_SIZE_VALID,
+                    validation_steps=valid_step_size,
                     epochs=10
 )
 
