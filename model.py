93327d24ea3dec821d97a62b0c6d84900230ed8a
ellafeldmann <ella_feldmann@brown.edu>
2019-04-30 15:48:27 -0400

    djkls

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet import preprocess_input
 import os
-from tesnsorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 
 
 img_width, img_height = 224, 224
