ed998de31c629e7a74183f0c81402ee575121fc4
ellafeldmann <ella_feldmann@brown.edu>
2019-05-02 18:40:57 -0400

    jkdljslk

--- evaluate_test_images.py
+++ evaluate_test_images.py
@@ -15,16 +15,14 @@ from tensorflow.keras.models import load_model
 test_dir = "/home/ella_feldmann/cv-final/test/"
 labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
 
-def predict(img):
 
-    loaded_model = tf.contrib.saved_model.load_keras_model('./tmp_dir/1556737843/')
+loaded_model = tf.contrib.saved_model.load_keras_model('./tmp_dir/1556737843/')
 
-    for filename in os.listdir(test_dir):
+for filename in os.listdir(test_dir):
     img = image.load_img(test_dir + filename)
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     preprocessed_image = applications.mobilenet.preprocess_input(img_array_expanded_dims)
     result = loaded_model.predict(preprocessed_image)
     print(filename)
-    print(np.shape(result))
     print(labels[np.argmax(result)])
