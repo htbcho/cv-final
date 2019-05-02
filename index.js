c0a26a06f5d9a73604ed54278da5b7df74159d8c
Helen Cho <helencho@helens-mbp.devices.brown.edu>
2019-05-02 14:59:16 -0400

    im trying

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // import * as tf from '@tensorflow/tfjs';
 // import Webcam from './webcam.js';
 
-
+const label = '';
 // const webcam = new Webcam(document.getElementById('webcamElement'));
 
-var getImage = function() {
+function getImage() {
     const video = document.querySelector("#webcamElement");
     var canvas = document.getElementById("canvas"); //if this doesn't work, try videoElement (previously container)
     var ctx = canvas.getContext('2d');
@@ -24,12 +24,27 @@ var draw = function(video, canvas, context, frameRate) {
     var url = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
     var image = new Image();
     image.src = url;
-
     //ok nowwww i think this will be ready for the model
+
+    getLabel(image);
+    var label = callbackFunc();
 
 
     setTimeout(draw, 1/frameRate, video, canvas, context, frameRate);
     //maybe push img to model here?
+}
+
+function getLabel(image) {
+    $.ajax({
+        type: 'POST',
+        url: 'load_test_model.py', //maybe no backlash
+        data: { param: image},
+        success: callbackFunc
+    });
+}
+
+function callbackFunc(response) {
+    label = response;
 }
 
 getImage();
--- load_test_model.py
+++ load_test_model.py
@@ -15,11 +15,12 @@ from tensorflow.keras.models import load_model
 test_dir = "/home/ella_feldmann/asl_alphabet_test/"
 labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
 
+def predict(img):
 
-loaded_model = tf.contrib.saved_model.load_keras_model('./tmp_dir/1556737843/')
+    loaded_model = tf.contrib.saved_model.load_keras_model('./tmp_dir/1556737843/')
 
-for filename in os.listdir(test_dir):
-    img = image.load_img(test_dir + filename)
+    # for filename in os.listdir(test_dir):
+    # img = image.load_img(test_dir + filename)
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     preprocessed_image = applications.mobilenet.preprocess_input(img_array_expanded_dims)
@@ -27,3 +28,4 @@ for filename in os.listdir(test_dir):
     print(filename)
     print(np.shape(result))
     print(labels[np.argmax(result)])
+    return labels[np.argmax(result)]
