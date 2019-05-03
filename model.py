9257fcd08d8d702c4032327f951e6a36838a4252
ellafeldmann <ella_feldmann@brown.edu>
2019-05-02 20:33:00 -0400

    model changes

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ train_generator = train_datagen.flow_from_directory(train_dir,
                                                  subset = 'training',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
-                                                 batch_size=20, # total number of training images should be divisible by batch size
+                                                 batch_size=50, # total number of training images should be divisible by batch size
                                                  class_mode='categorical',
                                                  shuffle=True)
 
@@ -56,7 +56,7 @@ valid_generator = train_datagen.flow_from_directory(train_dir,
                                                  subset = 'validation',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
-                                                 batch_size=20, # total number of training images should be divisible by batch size
+                                                 batch_size=50, # total number of training images should be divisible by batch size
                                                  class_mode='categorical',
                                                  shuffle=True)
 
