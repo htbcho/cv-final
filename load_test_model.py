5de0be1f6cac59b0bd8d3d6da868cf2b2bec53fe
Helen Cho <helencho@helens-mbp.devices.brown.edu>
2019-05-02 23:50:40 -0400

    set up flask server with stable connection

--- load_test_model.py
+++ load_test_model.py
@@ -1,31 +1,76 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Activation, Dense, GlobalAveragePooling2D
-from tensorflow.keras import optimizers
-from tensorflow.keras import models
-from tensorflow.keras import applications
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet import preprocess_input
+# import numpy as np
+# import tensorflow as tf
+# from tensorflow.keras import layers
+# from tensorflow.keras.models import Model
+# from tensorflow.keras.layers import Activation, Dense, GlobalAveragePooling2D
+# from tensorflow.keras import optimizers
+# from tensorflow.keras import models
+# from tensorflow.keras import applications
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.applications.mobilenet import preprocess_input
+# import os
+# from tensorflow.keras.models import load_model
+import requests 
+from flask import Flask, request, make_response, jsonify, render_template
+from flask_cors import CORS, cross_origin
 import os
-from tensorflow.keras.models import load_model
+
+
+app = Flask(__name__) #, static_folder='.'
+app._static_folder = os.path.abspath("templates/static/")
+CORS(app)
+
 
 test_dir = "/home/ella_feldmann/asl_alphabet_test/"
 labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
 
-def predict(img):
+prediction = ''
 
-    loaded_model = tf.contrib.saved_model.load_keras_model('./tmp_dir/1556737843/')
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('/index.html', title='Learn ASL!')
 
-    # for filename in os.listdir(test_dir):
-    # img = image.load_img(test_dir + filename)
-    img_array = image.img_to_array(img)
-    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-    preprocessed_image = applications.mobilenet.preprocess_input(img_array_expanded_dims)
-    result = loaded_model.predict(preprocessed_image)
-    print(filename)
-    print(np.shape(result))
-    print(labels[np.argmax(result)])
-    return labels[np.argmax(result)]
+
+@app.route('/model', methods = ['POST']) #what is a file path
+def test():
+    img = request.form['image']
+    ret = { 'label': 'im alive'}    
+    return jsonify(ret)
+# @app.route('/model', methods = ['POST']) #what is a file path
+# # @cross_origin()
+# def predict():
+#     web_image = request.form['image']
+#     print(web_image)
+#     loaded_model = tf.contrib.saved_model.load_keras_model('./tmp_dir/1556737843/')
+
+#     # for filename in os.listdir(test_dir):
+#     # img = image.load_img(test_dir + filename)
+#     img_array = image.img_to_array(img)
+#     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+#     preprocessed_image = applications.mobilenet.preprocess_input(img_array_expanded_dims)
+#     result = loaded_model.predict(preprocessed_image)
+#     print(filename)
+#     print(np.shape(result))
+#     print(labels[np.argmax(result)])
+#     prediction = labels[np.argmax(result)]
+#     if request.method == 'POST':
+#         result = prediction
+#         resp = make_response('{"response": '+prediction+'}')
+#         resp.headers['Content-Type'] = "application/json"
+#         return jsonify(resp)
+
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
+# r = requests.post('/index.js', data=prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True) #host='127.0.0.1', port=8000
---
+++ templates/static/js/index.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    const label = '';
+
+    // const webcam = new Webcam(document.getElementById('webcamElement'));
+
+    function getImage() {
+        const video = document.querySelector("#webcamElement");
+        var canvas = document.getElementById("canvas"); //if this doesn't work, try videoElement (previously container)
+        var ctx = canvas.getContext('2d');
+        video.addEventListener('play', draw(video, canvas, ctx, .0001));
+    }
+    // var capture = function() {
+    //     const video = document.querySelector("#webcamElement");
+    //     var canvas = document.getElementById("canvas"); //if this doesn't work, try videoElement (previously container)
+    //     var ctx = canvas.getContext('2d');
+    //     video.addEventListener('play', draw(video, canvas, ctx, 3));
+    // }
+
+    //https://github.com/tensorflow/tfjs-examples/blob/master/webcam-transfer-learning/index.js
+
+
+
+
+    //before was a var
+    function draw(video, canvas, context, frameRate) {
+        // console.log("draw");
+        // var topcanvas = document.getElementById("webcam-interaction");
+        var ret = context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        var url = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+        var image = new Image();
+        image.src = url;
+        //ok nowwww i think this will be ready for the model
+
+        getLabel(image);
+        // var label = callbackFunc();
+
+
+        setTimeout(draw, 1/frameRate, video, canvas, context, frameRate);
+        //maybe push img to model here?
+    }
+
+    function getLabel(image) { //eventually pass in letter <some set up stuff: https://stackoverflow.com/questions/11071100/jquery-uncaught-typeerror-illegal-invocation-at-ajax-request-several-eleme>
+        // $.ajax({
+            // beforeSend: function(xhrObj){
+            //     // xhrObj.setRequestHeader("Content-Type","application/json");
+            //     // xhrObj.setRequestHeader("Accept","application/json");
+            //     // xhrObj.setRequestHeader("Access-Control-Allow-Origin", "*");
+            //     xhrObj.setRequestHeader("Access-Control-Allow-Headers","x-requested-with");
+            // },
+        //     method: 'POST',
+        //     // context: elem,
+        //     url: '/load_test_model', //maybe no backlash
+        //     data: { param: image},
+            // crossDomain: true,
+            // processData: false,
+            // success: function(label) {
+            //     console.log('WORKINGGGGGGGG WORKING WORKING');
+            //     console.log(label);
+            //     // elem.innerHTML = TURN THE LETTER GREEN
+            // }
+        // });
+        $.post( "/model", {
+            // beforeSend: function(xhrObj){
+            //     // xhrObj.setRequestHeader("Content-Type","application/json");
+            //     // xhrObj.setRequestHeader("Accept","application/json");
+            //     // xhrObj.setRequestHeader("Access-Control-Allow-Origin", "*");
+            //     xhrObj.setRequestHeader("Access-Control-Allow-Headers","x-requested-with");
+            // },
+            // method: 'POST',
+            image: JSON.stringify('test string'),
+            crossDomain: true,
+            processData: false,
+            success: function(label) {
+                console.log('WORKINGGGGGGGG WORKING WORKING');
+                console.log(label);
+                // elem.innerHTML = TURN THE LETTER GREEN
+            },
+            function(response){ 
+                console.dir(response); 
+            }
+        });
+    }
+
+// function callbackFunc(response) {
+//     label = response;
+// }
+
+    getImage();
+});
