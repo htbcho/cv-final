f1414ffa02b225f445e26abc7ede92635f7c6f93
ellafeldmann <ella_feldmann@brown.edu>
2019-05-04 13:38:40 -0400

    jljkkljkl

---
+++ scratch_model_2.py
@@ -0,0 +1,92 @@
+import numpy as np
+np.random.seed(5)
+import tensorflow as tf
+tf.set_random_seed(2)
+import matplotlib.pyplot as plt
+%matplotlib inline
+import os
+import cv2
+import tensorflow.keras
+
+train_dir = "/home/ella_feldmann/asl_alphabet_train/"
+test_dir = "/home/ella_feldmann/asl_alphabet_test/"
+
+#Helper function to load images from given directories
+def load_images(directory):
+    images = []
+    labels = []
+    for idx, label in enumerate(uniq_labels):
+        for file in os.listdir(directory + "/" + label):
+            filepath = directory + "/" + label + "/" + file
+            image = cv2.resize(cv2.imread(filepath), (64, 64))
+            images.append(image)
+            labels.append(idx)
+    images = np.array(images)
+    labels = np.array(labels)
+    return(images, labels)
+
+
+
+uniq_labels = sorted(os.listdir(train_dir))
+images, labels = load_images(directory = train_dir)
+
+if uniq_labels == sorted(os.listdir(eval_dir)):
+    X_eval, y_eval = load_images(directory = eval_dir)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size = 0.1, stratify = labels)
+
+n = len(uniq_labels)
+train_n = len(X_train)
+test_n = len(X_test)
+
+print("Total number of symbols: ", n)
+print("Number of training images: " , train_n)
+print("Number of testing images: ", test_n)
+
+eval_n = len(X_eval)
+print("Number of evaluation images: ", eval_n)
+
+y_train_in = y_train.argsort()
+y_train = y_train[y_train_in]
+X_train = X_train[y_train_in]
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+y_eval = keras.utils.to_categorical(y_eval)
+
+X_train = X_train.astype('float32')/255.0
+X_test = X_test.astype('float32')/255.0
+X_eval = X_eval.astype('float32')/255.0
+
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, Dense, Dropout, Flatten
+from keras.layers import Flatten, Dense
+from keras.models import Sequential
+
+model = Sequential()
+model.add(Conv2D(filters = 64, kernel_size = 5, padding = 'same', activation = 'relu',
+                 input_shape = (64, 64, 3)))
+model.add(Conv2D(filters = 64, kernel_size = 5, padding = 'same', activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (4, 4)))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters = 128 , kernel_size = 5, padding = 'same', activation = 'relu'))
+model.add(Conv2D(filters = 128 , kernel_size = 5, padding = 'same', activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (4, 4)))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters = 256 , kernel_size = 5, padding = 'same', activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(29, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+hist = model.fit(X_train, y_train, epochs = 5, batch_size = 64)
+
+score = model.evaluate(x = X_test, y = y_test, verbose = 0)
+print('Accuracy for test images:', round(score[1]*100, 3), '%')
+score = model.evaluate(x = X_eval, y = y_eval, verbose = 0)
+print('Accuracy for evaluation images:', round(score[1]*100, 3), '%')
