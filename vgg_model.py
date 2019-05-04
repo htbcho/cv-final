509c83833621dee2e295e80027715d71bedb4940
ellafeldmann <ella_feldmann@brown.edu>
2019-05-03 22:15:32 -0400

    jkljkljkl

--- vgg_model.py
+++ vgg_model.py
@@ -39,9 +39,7 @@ x = Dense(256, activation='relu')(x)
 x = Dropout(0.5)(x)
 x = Dense(10, activation='softmax')(x)
 
-# Creating new model. Please note that this is NOT a Sequential() model.
-from keras.models import Model
-custom_model = Model(input=vgg_model.input, output=x)
+custom_model = models.Model(input=vgg_model.input, output=x)
 
 for layer in custom_model.layers[:7]:
     layer.trainable = False
