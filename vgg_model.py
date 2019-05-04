2c403b01ad666ffee600fc3009f184ddac15fbf9
ellafeldmann <ella_feldmann@brown.edu>
2019-05-04 11:37:15 -0400

    jljklj

--- vgg_model.py
+++ vgg_model.py
@@ -9,7 +9,6 @@ from tensorflow.keras import models
 from tensorflow.keras import applications
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import reshape
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input
