c6bd5a46d6c0166a5699d438398afb4ef94e671b
ellafeldmann <ella_feldmann@brown.edu>
2019-05-05 18:50:06 -0400

    JKLJKLJKL

--- all_models/vgg_model.py
+++ all_models/vgg_model.py
@@ -45,34 +45,25 @@ vgg_model.trainable = False
 print('This is the number of trainable weights '
       'after freezing the conv base:', len(model.trainable_weights))
 
-model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
-              loss='categorical_crossentropy',
-              metrics=['acc'])
 
 
-train_datagen = ImageDataGenerator(
-      rescale=1./255,
-      width_shift_range=0.1,
-      height_shift_range=0.1,
-      zoom_range=0.2,
-      fill_mode='nearest')
+train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.2)
 
-# Note that the validation data should not be augmented!
-test_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                 subset = 'training',
+                                                 target_size=(224,224),
+                                                 color_mode='rgb',
+                                                 batch_size=20, # total number of training images should be divisible by batch size
+                                                 class_mode='categorical',
+                                                 shuffle=True)
 
-train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        subset = 'training',
-        target_size=(224, 224),
-        batch_size=20,
-        class_mode='categorical')
-
-validation_generator = test_datagen.flow_from_directory(
-        train_dir,
-        subset = 'validation',
-        target_size=(224, 224),
-        batch_size=20,
-        class_mode='categorical')
+valid_generator = train_datagen.flow_from_directory(train_dir,
+                                                 subset = 'validation',
+                                                 target_size=(224,224),
+                                                 color_mode='rgb',
+                                                 batch_size=20, # total number of training images should be divisible by batch size
+                                                 class_mode='categorical',
+                                                 shuffle=True)
 
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.RMSprop(lr=2e-5),
@@ -82,102 +73,14 @@ history = model.fit_generator(
       train_generator,
       steps_per_epoch=100,
       epochs=30,
-      validation_data=validation_generator,
+      validation_data=valid_generator,
       validation_steps=50,
       verbose=2)
-#
-# #
-# # from keras import models
-# # from keras import layers
-# # from keras import optimizers
-# #
-# # model = models.Sequential()
-# # model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
-# # model.add(layers.Dropout(0.5))
-# # model.add(layers.Dense(1, activation='sigmoid'))
-# #
-# # model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
-# #               loss='binary_crossentropy',
-# #               metrics=['acc'])
-# #
-# # history = model.fit(train_features, train_labels,
-# #                     epochs=30,
-# #                     batch_size=20,
-# #                     validation_data=(validation_features, validation_labels))
-#
-#
-# for layer in model.layers:
-#     layer.trainable=False
-# for layer in model.layers[-2:]:
-#     layer.trainable=True
-#
-# opt = RMSprop(lr=0.001)
-# model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-#
-#  # fit_model(model, batches, val_batches, 2)
-#
-#
-#
-# # # Getting output tensor of the last VGG layer that we want to include
-# # # x = layer_dict['block2_pool'].output
-# # x = vgg_model.output
-# # # Stacking a new simple convolutional network on top of it
-# # # x = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(x)
-# # x = MaxPooling2D(pool_size=(2, 2))(x)
-# # x = Flatten()(x)
-# # x = Dense(256, activation='relu')(x)
-# # x = Dropout(0.5)(x)
-# # x = Dense(29, activation='softmax')(x)
-# #
-# # custom_model=Model(inputs=vgg_model.input,outputs=x)
-# #
-# # for layer in custom_model.layers[:7]:
-# #     layer.trainable = False
-# # for layer in custom_model.layers[7:]:
-# #     layer.trainable = True
-# #
-# # # custom_model.compile(loss='categorical_crossentropy',
-# # #                      optimizer='rmsprop',
-# # #                      metrics=['accuracy'])
-#
-#
-# train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.2)
-#
-# train_generator = train_datagen.flow_from_directory(train_dir,
-#                                                  subset = 'training',
-#                                                  target_size=(224,224),
-#                                                  color_mode='rgb',
-#                                                  batch_size=20, # total number of training images should be divisible by batch size
-#                                                  class_mode='categorical',
-#                                                  shuffle=True)
-#
-# valid_generator = train_datagen.flow_from_directory(train_dir,
-#                                                  subset = 'validation',
-#                                                  target_size=(224,224),
-#                                                  color_mode='rgb',
-#                                                  batch_size=20, # total number of training images should be divisible by batch size
-#                                                  class_mode='categorical',
-#                                                  shuffle=True)
-#
-#
-# train_step_size=train_generator.n//train_generator.batch_size
-# valid_step_size=valid_generator.n//valid_generator.batch_size
-#
-# history = model.fit_generator(generator=train_generator,
-#                     steps_per_epoch=train_step_size,
-#                     validation_data=valid_generator,
-#                     validation_steps=valid_step_size,
-#                     epochs=5
-# )
-#
-#
-# def preprocess_image(img):
-#     img = image.load_img(train_dir + "/" + img)
-#     img = image.img_to_array(img)
-#     img = np.expand_dims(img, axis=0)
-#     img = preprocess_input(img)
-#     return img
-# #
+
+
+
+
+
 # # for subdir in os.listdir(test_dir):
 # #
 # #     for filename in os.listdir(test_dir + subdir):
