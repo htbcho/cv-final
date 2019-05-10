b4e244820fed72819689409e76b7eedd27503f0e
Helen Cho <helencho@helens-mbp.devices.brown.edu>
2019-05-10 12:39:05 -0400

    deleted comments, cleaned up code

--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -5,13 +5,10 @@ $(document).ready(function(){
     const word_bank = ["HOW"];
     let word;
     let all_letters;
-    // let curr_letter;
     let curr_index;
 
-    // document.getElementById("new-word").addEventListener("onclick", newWord());
     var button = document.getElementById("new-word");
     button.onclick = function() {
-        // var container = $("#word-container");
         var wrapper = $("#center-wrapper");
         wrapper.empty();
         var max = word_bank.length-1;
@@ -40,53 +37,34 @@ $(document).ready(function(){
         }
         
         curr_index = 0;
-        // curr_letter = word.charAt(0);
-        // l.textContent.charAt(0).fontcolor = "#42a4f4";
-        // console.log(l.textContent.charAt(0).fontcolor(getColor()));
-        // console.log('asdada');
     }
     
 
-
     function getImage() {
         const video = document.querySelector("#webcamElement");
-        var canvas = document.getElementById("canvas"); //if this doesn't work, try videoElement (previously container)
+        var canvas = document.getElementById("canvas"); 
         var ctx = canvas.getContext('2d');
         video.addEventListener('play', draw(video, canvas, ctx, .0001));
-        //set width and height of mirror?????
     }
-    //https://github.com/tensorflow/tfjs-examples/blob/master/webcam-transfer-learning/index.js
 
-    //before was a var
     function draw(video, canvas, context, frameRate) {
-        // console.log("draw");
-        // var topcanvas = document.getElementById("webcam-interaction");
         var ret = context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        var url = canvas.toDataURL("image/jpeg"); //.replace("image/jpeg", "image/octet-stream")
+        var url = canvas.toDataURL("image/jpeg"); 
         var image = document.getElementById("mirror");
         image.src = url;
 
         pushImage(image);
 
-
         setTimeout(draw, 1/frameRate, video, canvas, context, frameRate);
-        //maybe push img to model here?
     }
 
-    function pushImage(image) { //eventually pass in letter <some set up stuff: https://stackoverflow.com/questions/11071100/jquery-uncaught-typeerror-illegal-invocation-at-ajax-request-several-eleme>
+    function pushImage(image) { 
         $.post( "/model", {
             url: image.src,
             crossDomain: true,
             processData: false,
             success: changeLetter()
             }, 
-            // function(err, req, response){ 
-            //     // getLabel();
-            //     // console.log("done POST");
-            //     // console.log(response["responseJSON"]["label"]);
-            //     // var label = response["responseJSON"]["label"]; 
-            //     // changeLetter();
-            // }
             );
     }
 
