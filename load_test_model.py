4acf0082f09e1e87525ca9f851be1c4f46d0952c
ellafeldmann <ella_feldmann@brown.edu>
2019-05-10 12:48:38 -0400

    final changes to load_test_model.py

--- load_test_model.py
+++ load_test_model.py
@@ -56,7 +56,8 @@ def predict():
     # global start
     if start == 1:
         urllib.request.urlretrieve(url, 'webcam_images/curr.jpg')
-        img_array = image.img_to_array(img, target_size = (64,64))
+        img = image.load_img('webcam_images/curr.jpg', target_size=(64,64))
+        img_array = image.img_to_array(img)
         img_array = np.divide(img_array, 255.0)
         img_array_expanded_dims = np.expand_dims(img_array, axis=0)
         loaded_model = tf.contrib.saved_model.load_keras_model('./scratch_tmp_dir/1556848947/') # THE WORKING MODEL
